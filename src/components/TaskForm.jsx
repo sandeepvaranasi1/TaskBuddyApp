cec1f7b0bba6f13e466a83fd2576df299aeaa704
sandeepvaranasi1 <sandeepvaranasi568@gmail.com>
2024-12-30 10:55:13 +0530

    Added Styling

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,27 +1,48 @@
 import React, { useState } from "react";
 
-export default function TaskForm() {
+export default function TaskForm({ addTask }) {
   const [task, setTask] = useState("");
   const [priority, setPriority] = useState("Medium");
   const [category, setCategory] = useState("General");
-
+  const handlesubmit = (e) => {
+    e.preventDefault();
+    addTask({ text: task, priority, category, completed: false }); //send data to addTask()
+    //Reset state
+    setPriority("Medium");
+    setCategory("General");
+    setTask("");
+  };
   return (
-    <form className="task-form">
+    <form onSubmit={handlesubmit} className="task-form">
       <div id="inp">
         <input
           type="text"
-          name="task"
-          id="task"
           placeholder="Enter Your Task"
           required
           value={task}
           onChange={(e) => setTask(e.target.value)}
         />
-        <span><button>Add Task</button></span>
-      </div>
 
-      <div>
+        <div className="btns">
+          <select
+            value={priority}
+            onChange={(e) => setPriority(e.target.value)}>
+            <option>Medium</option>
+            <option>Low</option>
+            <option>High</option>
+          </select>
 
+          <select
+            value={category}
+            onChange={(e) => setCategory(e.target.value)}>
+            <option>General</option>
+            <option>Work</option>
+            <option>Personal</option>
+          </select>
+          <span>
+            <button>Add Tasks</button>
+          </span>
+        </div>
       </div>
     </form>
   );
